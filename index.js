4dcdb4c0c2f40bae6e5ca7b3f8523d02401bff6e
Pragati Chothe <pragatichothe@gmail.com>
2022-10-25 13:06:08 +0530

    try to build a full stack Application and deploy

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ process.emit("exist");
 //file system (sync==blocking)
 const { readFile, readFileSync } = require("fs");
 const txt = readFileSync("./hello.txt", "utf8"); //if you don't use utf8 it will show some buffer instead of sentence
-console.log(txt,"1st one");
+console.log(txt, "1st one");
 
 //you can also right like this (and this will run after that)
 readFile("./hello.txt", "utf8", (err, txt) => {
@@ -55,9 +55,40 @@ async function hello() {
 }
 
 //import module
-const myModule=require('./module')
-console.log(myModule)
+const myModule = require("./module");
+console.log(myModule);
 
 //npm init -y  -- to install default package module
-//npm install express  -- install express 
+//npm install express  -- install express
 
+/* 
+
+  To build a real full stack Application and deploy it to the cloud
+  Server will live on a url and when a user makes a request to this url in the browser the server will respond with some html 
+  In our code we'll first create an instance of an express app 
+  An express app allows us to create different urls and endpoints that a user can navigate to in the browser 
+  And then we define code for the server to handle those requests
+
+*/
+
+const express = require("express");
+const { response } = require("express");
+const app = express();
+
+//when the user navigates to url in the browser it known as get request
+//first argument is the url that the user will navigate to in this case we'll just use a forward slash for the root url
+app.get("/foo/bar");
+
+//second argument here is our callback function you can think of every request to this url as an event and then you handle that event with this function and express gives us two parameters to make use of the request and the response the request is the incoming data from the user
+//request you incoming data
+//response your incomming data
+app.get('/',(request,response)=>{
+  readFile('./index.html', 'utf8', (err,html)=>{
+    if(err){
+      response.status(500).send('Sorry, Out of Order')
+    }
+    response.send(html)
+  })
+})
+
+app.listen(process.env.Port || 3000, ()=> console.log(" App is Available now on http://localhost:3000 !!!"))
