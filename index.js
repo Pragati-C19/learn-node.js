546f9716e904f86dd76df1fa169bf72ca2b4c96f
Pragati Chothe <pragatichothe@gmail.com>
2022-10-26 13:02:17 +0530

    OS Module

--- index.js
+++ index.js
@@ -147,7 +147,6 @@ const sayHi=require("./sayhi-function")
 const data=require("./alternative-syntax")
 require("./mind-grenade")
 
-
 console.log(names)
 console.log(data)
 console.log(data.singlePerson)
@@ -156,3 +155,26 @@ console.log(data.items)
 sayHi("Susan");
 sayHi(names.john);
 sayHi(names.peter)
+
+//Build-In Modules
+// 1. OS
+
+const os = require('os')
+//info about current user
+const user = os.userInfo()
+console.log(user)
+
+//method returns the system uptime in seconds
+//Uptime is a command that returns information about how long your system has been running together with the current time
+console.log(`The system Uptime is ${os.uptime()} seconds`)
+
+const currentOS={
+  name : os.type(),
+  release : os.release(),
+  totalMemory : os.totalmem(),
+  freeMemory: os.freemem(),
+}
+
+console.log(currentOS)
+
+
