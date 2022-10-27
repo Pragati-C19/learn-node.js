43efd40ac525cd320a6d3669ba6aded643341c9d
Pragati Chothe <pragatichothe@gmail.com>
2022-10-27 12:51:23 +0530

    Node's Native Option

---
+++ 12-async-pattern-native.js
@@ -0,0 +1,24 @@
+// Async Patterns
+// 4. Node's Native Option 
+
+const { readFile, writeFile } = require("fs").promises;
+
+// const util = require('util')
+// const readFilePromise= util.promisify(readFile)
+// const writeFilePromise= util.promisify(writeFile)
+
+const start = async () => {
+  try {
+    const first = await readFile("./content/first.txt", "utf8");
+    const second = await readFile("./content/second.txt", "utf8");
+    await writeFile(
+      "./content/result-mind-grenade.txt",
+      `This is Awesome : ${first} ${second}`,
+      { flag: "a" }
+    );
+    console.log(first, second, "In try Catch method");
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
