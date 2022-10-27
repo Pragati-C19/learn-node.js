0835f1d7a2cfed5b7603fa6314c082e53e6b980d
Pragati Chothe <pragatichothe@gmail.com>
2022-10-27 17:39:33 +0530

    additional info about streams

---
+++ 16-streams-additional-info.js
@@ -0,0 +1,17 @@
+//Streams
+//2. Additional Info
+
+const { createdSream, createReadStream } = require("fs");
+
+const stream = createReadStream("./content/big.txt", {
+  highWaterMark: 9000,
+  encoding: "utf8",
+});
+
+stream.on("data", (result) => {
+  console.log(result);
+});
+
+stream.on("error", (result) => {
+  console.log("error");
+});
