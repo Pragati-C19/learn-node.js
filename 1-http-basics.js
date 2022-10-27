74167e5e15a2351a65f24fd8f01f6a3aa3be5f6e
Pragati Chothe <pragatichothe@gmail.com>
2022-10-27 18:29:39 +0530

    http basics

---
+++ 1-http-basics.js
@@ -0,0 +1,10 @@
+// To exchange the data use request and response
+//port is communication endport
+
+const http=require('http')
+const server=http.createServer((req,res)=>{
+    console.log("User Hit the Server") // this will show on cmd only
+    res.end("Home Page") //this shows on browser
+})
+
+server.listen(5000)
