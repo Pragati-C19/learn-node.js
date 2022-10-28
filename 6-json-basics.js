a2809bf799c0c194a0c53dd8c6b5a20bcc1b651d
Pragati Chothe <pragatichothe@gmail.com>
2022-10-28 14:34:38 +0530

    json basics

---
+++ 6-json-basics.js
@@ -0,0 +1,20 @@
+/*
+        API         Vs          SSR
+    API - json              SSR- Template 
+    SEND Data               SEND Template
+    res.json()              res.render()
+*/
+
+const express = require("express");
+const app = express();
+const {products} =require('./data')
+
+
+app.get("/", (req, res) => {
+  //res.json([{ name: "Pragati" }, { name: "Kiran" }]);
+    res.json(products)
+});
+
+app.listen(5000, () => {
+  console.log("Server is Running on Port 5000...");
+});
